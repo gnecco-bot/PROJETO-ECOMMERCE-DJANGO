06ae6db058e781bf3d3dbac5b6904691f98cba4b
Lucas <lucasalvesgnecco@gmail.com>
2025-12-30 11:44:04 -0300

    Models do pedido & itempedido

--- produto/admin.py
+++ produto/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from . import models
 
-class VariacaoInLine(admin.StackedInline):
+class VariacaoInLine(admin.TabularInline):
     model = models.Variacao
     extra = 1
 
 class ProdutoAdmin(admin.ModelAdmin):
-    inLines = [VariacaoInLine]
+    inlines = [
+        VariacaoInLine
+        ]
 
 admin.site.register(models.Produto, ProdutoAdmin)
 admin.site.register(models.Variacao)
