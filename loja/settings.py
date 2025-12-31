997f761c88dbaa0d720948130f4a833ffa374b63
Lucas <lucasalvesgnecco@gmail.com>
2025-12-31 11:05:37 -0300

    Formatacao preco, CSS & html

--- loja/settings.py
+++ loja/settings.py
@@ -129,9 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'templates/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join('static')
+    os.path.join('templates/static')
 ]
 
 MEDIA_URL = '/media/'
